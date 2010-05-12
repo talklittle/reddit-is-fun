cdfbaed9fca424ba59a7f1d3cc70b3a866c280f8
Andrew Shu <talklittle@gmail.com>
2010-05-12 00:30:35 -0400

    inbox: add back hack to mark messages read w/o clicking

--- src/com/andrewshu/android/reddit/InboxActivity.java
+++ src/com/andrewshu/android/reddit/InboxActivity.java
@@ -426,6 +426,11 @@ public final class InboxActivity extends ListActivity
                 pin.close();
                 in.close();
                 
+                // XXX: HACK: http://code.reddit.com/ticket/709
+                // Marking messages as read is currently broken (even with mark=
+                // For now, just send an extra request to the regular non-JSON i
+                mClient.execute(new HttpGet("http://www.reddit.com/message/inbox"));
+
             } catch (Exception e) {
             	if (Constants.LOGGING) Log.e(TAG, "failed:" + e.getMessage());
         	} finally {
