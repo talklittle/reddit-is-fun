61504e479a02aa6db904d2ec3b708a331cad232d
Colin Edwards <ddrboxman@gmail.com>
2010-06-25 06:31:27 +0800

    Inbox Captcha size enlarge

--- src/com/andrewshu/android/reddit/InboxActivity.java
+++ src/com/andrewshu/android/reddit/InboxActivity.java
@@ -52,7 +52,11 @@ import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -931,7 +935,21 @@ public final class InboxActivity extends ListActivity
 	    	
 				InputStream in = response.getEntity().getContent();
 				
-				return Drawable.createFromStream(in, "captcha");
+				//get image as bitmap
+				Bitmap captchaOrg  = BitmapFactory.decodeStream(in);
+
+				// create matrix for the manipulation
+				Matrix matrix = new Matrix();
+				// resize the bit map
+				matrix.postScale(2f, 2f);
+
+				// recreate the new Bitmap
+				Bitmap resizedBitmap = Bitmap.createScaledBitmap (captchaOrg,
+						captchaOrg.getWidth() * 3, captchaOrg.getHeight() * 3, true);
+			 
+				BitmapDrawable bmd = new BitmapDrawable(resizedBitmap);
+				
+				return bmd;
 			
 			} catch (Exception e) {
 				Common.showErrorToast("Error downloading captcha.", Toast.LENGTH_LONG, InboxActivity.this);
