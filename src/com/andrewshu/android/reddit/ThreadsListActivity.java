b17913e559626ed3591fc8c2ce0e38be776cc299
Andrew Shu <talklittle@gmail.com>
2010-12-20 19:53:37 -0500

    threads: confirm quit, if isTaskRoot()
    http://stackoverflow.com/questions/2257963/android-how-to-show-dialog-to-confirm-user-wishes-to-exit-activity
    
    plus isTaskRoot() check

--- src/com/andrewshu/android/reddit/ThreadsListActivity.java
+++ src/com/andrewshu/android/reddit/ThreadsListActivity.java
@@ -48,6 +48,7 @@ import android.text.style.ForegroundColorSpan;
 import android.text.style.TextAppearanceSpan;
 import android.util.Log;
 import android.view.ContextMenu;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -258,6 +259,34 @@ public final class ThreadsListActivity extends ListActivity {
     	default:
     		break;
     	}
+    }
+    
+    /**
+     * http://stackoverflow.com/questions/2257963/android-how-to-show-dialog-to-confirm-user-wishes-to-exit-activity
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //Handle the back button
+        if(keyCode == KeyEvent.KEYCODE_BACK && isTaskRoot()) {
+            //Ask the user if they want to quit
+            new AlertDialog.Builder(this)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setTitle(R.string.quit)
+            .setMessage(R.string.really_quit)
+            .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    //Stop the activity
+                    finish();    
+                }
+            })
+            .setNegativeButton(R.string.no, null)
+            .show();
+
+            return true;
+        }
+        else {
+            return super.onKeyDown(keyCode, event);
+        }
     }
     
     
