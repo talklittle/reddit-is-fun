52bceb7c7c84a6c885b925e11fbd4c546a71a1c8
Andrew Shu <talklittle@gmail.com>
2011-12-04 05:43:24 -0500

    fix a comment

--- src/com/andrewshu/android/reddit/mail/InboxListActivity.java
+++ src/com/andrewshu/android/reddit/mail/InboxListActivity.java
@@ -563,7 +563,7 @@ public final class InboxListActivity extends ListActivity
                 parseInboxJSON(pin);
                 
                 // XXX: HACK: http://code.reddit.com/ticket/709
-                // Marking messages as read is currently broken (even with mark=false)
+                // Marking messages as read is currently broken (even with mark=true)
                 // For now, just send an extra request to the regular non-JSON inbox
                 mClient.execute(new HttpGet(Constants.REDDIT_BASE_URL + "/message/" + mWhichInbox));
 
